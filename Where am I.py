b2dead83a66241b2bd9faca596a72d210a87c050
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-01-03 16:46:57 +0600

    Create Where am I.py

---
+++ Where am I.py
@@ -0,0 +1,19 @@
+#Navigate to a specific element of XPath notation:
+
+Consider the HTML code:
+
+<html>
+  <body>
+    <div>
+      <p>Good Luck!</p>
+      <p>Not here...</p>
+    </div>
+    <div>
+      <p>Where am I?</p>
+    </div>
+  </body>
+</html>
+
+Code:
+  # Fill in the blank
+  xpath = '/html/body/div[2]/p'
